ab5bbc998b6074fcf115889bfa412876dd302244
acbeni <beni.asaad.achraf@gmail.com>
2021-07-17 18:12:41 +0200

    move all to one folder

--- arduiapp/app/src/main/java/com/targis/magicembed/di/AppContainer.kt
+++ arduiapp/app/src/main/java/com/targis/magicembed/di/AppContainer.kt
@@ -2,7 +2,6 @@ package com.targis.magicembed.di
 
 import android.hardware.usb.UsbManager
 import com.targis.magicembed.device.AppBroadcastReceiver
-//import com.targis.magicembed.device.UsbDeviceClient
 import com.targis.magicembed.room.AppDatabase
 
 data class AppContainer(
