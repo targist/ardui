1309efa20ff1b95d17c3549ed4bf4bb497415142
yousfi saad <yousfi.saad@gmail.com>
2021-09-03 15:48:31 +0100

    Allow user to select port from the UI

---
+++ react-serial-ui/src/components/ChangePort.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+import { Port } from '../hooks/useLogs';
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+});
+
+export interface ChangePortDialogProps {
+  availablePorts: Port[];
+  open: boolean;
+  selectedPort: Port | null;
+  onClose: (value: Port | null) => void;
+}
+
+function ChangePortDialog(props: ChangePortDialogProps) {
+  const classes = useStyles();
+  const { onClose, selectedPort, open, availablePorts } = props;
+
+  const handleClose = () => {
+    onClose(selectedPort);
+  };
+
+  const handleListItemClick = (value: Port) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Select Port</DialogTitle>
+      <List>
+        {availablePorts.map((port) => (
+          <ListItem
+            button
+            onClick={() => handleListItemClick(port)}
+            key={port.path}
+          >
+            <ListItemText primary={port.path} />
+          </ListItem>
+        ))}
+      </List>
+    </Dialog>
+  );
+}
+export type ChangePortProps = {
+  selectedPort: Port | null;
+  setSelectedPort: (port: Port | null) => void;
+  availablePorts: Port[];
+};
+export default function ChangePort({ selectedPort, setSelectedPort, availablePorts }: ChangePortProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value: Port | null) => {
+    setOpen(false);
+    setSelectedPort(value);
+  };
+
+  return (
+    <>
+      <Button style={{
+        float: "right",
+      }} variant="outlined" color="default" onClick={handleClickOpen}>
+        Change Port
+      </Button>
+      <ChangePortDialog availablePorts={availablePorts} selectedPort={selectedPort} open={open} onClose={handleClose} />
+    </>
+  );
+}
--- react-serial-ui/src/hooks/useLogs.ts
+++ react-serial-ui/src/hooks/useLogs.ts
@@ -1,10 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-export default function useLogs() {
+export type Port = {
+  locationId?: string;
+  manufacturer?: string;
+  path?: string;
+  productId?: string;
+  serialNumber?: string;
+};
+
+export default function useLogs(port?: string) {
+  const [availablePorts, setAvailablePorts] = useState<Port[]>([]);
+  const [selectedPort, setSelectedPort] = useState<Port | null>(null);
   const [logs, setLogs] = useState<any[]>([]);
   const [connected, setConnected] = useState(false);
-  const emit = useRef<((ev: any, data: any) => void) | null>(null);
+  const emit = useRef<((ev: any, data?: any) => void) | null>(null);
 
   useEffect(() => {
     const socket = io(location.host);
@@ -22,18 +32,44 @@ export default function useLogs() {
       else setLogs([allLogs]);
     });
 
-    emit.current = (ev: any, data: any) => {
-      socket.emit(ev, data);
+    socket.on("available-ports", (data) => {
+      setAvailablePorts(data);
+    });
+
+    socket.on("port-selected", (data) => {
+      setSelectedPort(data);
+    });
+
+    emit.current = (ev: any, data?: any) => {
+      data ? socket.emit(ev, data) : socket.emit(ev);
     };
   }, []);
 
   const commands = {
     upload: (data: any) => emit.current && emit.current("upload", data),
+    sendGetAvailablePortsReq: () =>
+      emit.current && emit.current("get-available-ports"),
+    sendSelectPortReq: (port: string) =>
+      emit.current && emit.current("select-port", port),
   };
+
+  useEffect(() => {
+    if (connected) {
+      commands.sendGetAvailablePortsReq();
+    }
+  }, [connected]);
+
+  useEffect(() => {
+    if (connected && port) {
+      commands.sendSelectPortReq(port);
+    }
+  }, [port]);
 
   return {
     connected,
     logs,
     commands,
+    selectedPort,
+    availablePorts,
   };
 }
